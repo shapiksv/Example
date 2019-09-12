bfcbd5912f265ed1b3b0f15bdf7761a6f94f585d
shapiksv <51991973+shapiksv@user.noreply.github.com>
2019-09-12 07:32:18 +0300

    Initial commit

---
+++ src/ThreadSwapping.java
@@ -0,0 +1,49 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class ThreadSwapping {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        //запоминаем настоящий PrintStream в специальную переменную
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String result = outputStream.toString();
+
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+
+        //разворачиваем строку
+        int i = 0;
+        String[] newString = result.split("\\n");
+        for(String s : newString){
+            System.out.println(s);
+            i +=1;
+            if(i%2 == 0){
+
+                System.out.println("JavaRush - курсы Java онлайн");
+            }
+        }
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
